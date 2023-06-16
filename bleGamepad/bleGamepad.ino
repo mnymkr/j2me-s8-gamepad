323965852e017f648278d7ca29f8d0314084911d
mnymkr <hoang.tran.200032@student.fulbright.edu.vn>
2023-06-16 20:25:49 +0700

    tried to change mapping so that it made more sense, but failed instead

--- bleGamepad/bleGamepad.ino
+++ bleGamepad/bleGamepad.ino
@@ -1,27 +1,3 @@
-/*
- * This example turns the ESP32 into a Bluetooth LE gamepad that presses buttons and moves axis
- *
- * At the moment we are using the default settings, but they can be canged using a BleGamepadConfig instance as parameter for the begin function.
- *
- * Possible buttons are:
- * BUTTON_1 through to BUTTON_16
- * (16 buttons by default. Library can be configured to use up to 128)
- *
- * Possible DPAD/HAT switch position values are:
- * DPAD_CENTERED, DPAD_UP, DPAD_UP_RIGHT, DPAD_RIGHT, DPAD_DOWN_RIGHT, DPAD_DOWN, DPAD_DOWN_LEFT, DPAD_LEFT, DPAD_UP_LEFT
- * (or HAT_CENTERED, HAT_UP etc)
- *
- * bleGamepad.setAxes sets all axes at once. There are a few:
- * (x axis, y axis, z axis, rx axis, ry axis, rz axis, slider 1, slider 2)
- *
- * Library can also be configured to support up to 5 simulation controls
- * (rudder, throttle, accelerator, brake, steering), but they are not enabled by default.
- *
- * Library can also be configured to support different function buttons
- * (start, select, menu, home, back, volume increase, volume decrease, volume mute)
- * start and select are enabled by default
- */
-
 #include <Arduino.h>
 #include <BleGamepad.h>
 
@@ -72,8 +48,6 @@ void setup()
     int min = bleGamepadConfig.getAxesMin();
     int max = bleGamepadConfig.getAxesMax();
 
-    Serial.begin(115200);
-    Serial.println("Starting BLE work!");
     bleGamepadConfig.setButtonCount(numOfButtons);
     bleGamepadConfig.setControllerType(CONTROLLER_TYPE_GAMEPAD);
     bleGamepadConfig.setWhichSpecialButtons(true, true, true, true, true, true, true, true);
